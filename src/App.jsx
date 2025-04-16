ba4cdbbbdeda271c0741730c1b1bbf70b660ae89
Riccardo Delrio <riccardodelrio92@gmail.com>
2025-04-16 12:01:22 +0200

    Add routing and layout structure; create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import DefaultLayout from "./layout/defaultLayout";
 
 function App() {
 
   return (
     <>
-      
+            <Router>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+
+          </Route>
+        </Routes>
+      </Router>
      
     </>
   )
